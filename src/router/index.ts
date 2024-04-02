6369be0d2413d408f7fe99354de73703760a9279
youlimsongs <giuhki@gmail.com>
2024-04-03 03:38:12 +0900

    feat: 배경layout, not-found 페이지 설정

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import MainPageVue from '../pages/MainPage.vue';
 import PopUpVue from '../pages/PopUp.vue';
+import MainLayoutVue from '../layout/MainLayout.vue';
+import NotFounVue from '../pages/NotFoun.vue';
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/main', name: 'main', component: MainPageVue },
+    {
+      path: '/',
+      name: 'home',
+      component: MainLayoutVue,
+      children: [{ path: '/main', name: 'main', component: MainPageVue }],
+    },
     { path: '/popup', name: 'popup', component: PopUpVue },
+    {
+      path: '/:pathMatch(.*)*',
+      component: NotFounVue,
+    },
   ],
 });
 
