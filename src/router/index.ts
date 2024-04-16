6114b41e3c82756ff88573d323e7d0aac3ae9956
miyoung12 <miseja3@gmail.com>
2024-04-16 15:52:34 +0900

    feat:텍스트 에디터 구현

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,30 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import MainPageVue from '../pages/MainPage.vue';
-import PopUpVue from '../pages/PopUp.vue';
-import MainLayoutVue from '../layout/MainLayout.vue';
-import NotFoundVue from '../pages/NotFound.vue';
-import DetailPage from '../pages/DetailPage.vue';
-import PostPage from '../pages/PostPage.vue';
-import TextEditor from '../components/TextEditor.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import MainPageVue from "../pages/MainPage.vue";
+import PopUpVue from "../pages/PopUp.vue";
+import MainLayoutVue from "../layout/MainLayout.vue";
+import NotFoundVue from "../pages/NotFound.vue";
+import DetailPage from "../pages/DetailPage.vue";
+import PostPage from "../pages/PostPage.vue";
+import Editor from "../components/Editor.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: MainLayoutVue,
       children: [
         //배경 layout 적용할 페이지 경로 설정
-        { path: '/main', name: 'main', component: MainPageVue },
-        { path: '/detail', name: 'detail-page', component: DetailPage },
-        { path: '/post', name: 'post', component: PostPage },
-        { path: '/edit', name: 'edit', component: TextEditor },
+        { path: "/main", name: "main", component: MainPageVue },
+        { path: "/detail", name: "detail-page", component: DetailPage },
+        { path: "/post", name: "post", component: PostPage },
+        { path: "/edit", name: "edit", component: Editor },
       ],
     },
-    { path: '/popup', name: 'popup', component: PopUpVue },
+    { path: "/popup", name: "popup", component: PopUpVue },
     {
-      path: '/:pathMatch(.*)*',
+      path: "/:pathMatch(.*)*",
       component: NotFoundVue,
     },
   ],
